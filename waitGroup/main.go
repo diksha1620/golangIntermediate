58ffe97b43a36857f90671b565037b93e61a8b15
diksha1620 <dikshabhutekar@nimapinfotech.com>
2023-08-18 16:59:52 +0530

    waitGroup

---
+++ waitGroup/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	add(12, 114)
+	go add(22, 33)
+	wg.Wait()
+
+}
+
+func add(a int, b int) int {
+	defer wg.Done()
+	c := a + b
+	return c
+
+}
